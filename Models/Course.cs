727f4abac37c5c84bded7f1f15e73e119fbb448c
Fábio Vicente <fab10_lima@hotmail.com>
2022-12-14 23:03:35 -0300

    refactor: :truck: rename `Models` namespace

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace CRMEducacional.models;
+namespace CRMEducacional.Models;
 
 public class Course
 {
--- Models/Registration.cs
+++ Models/Registration.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
-namespace CRMEducacional.models;
+namespace CRMEducacional.Models;
 
 public class Registration
 {
