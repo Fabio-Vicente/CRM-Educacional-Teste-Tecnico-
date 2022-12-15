5b843853f6e699a4f2853f3406a223682d73471f
Fábio Vicente <fab10_lima@hotmail.com>
2022-12-14 23:31:59 -0300

    feat(Context): :sparkles: inject context dependency

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using CRMEducational.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<CRMEducacionalContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
