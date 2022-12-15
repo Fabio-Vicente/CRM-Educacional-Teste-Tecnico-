290a08e4abf5abe62979edd7e3d11e71c3547c09
Fábio Vicente <fab10_lima@hotmail.com>
2022-12-14 22:53:56 -0300

    feat(Lead): :sparkles: create model

---
+++ models/Lead.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CRMEducacional.models;
+
+public class Lead
+{
+  public int Id { get; set; }
+  [MaxLength(50)]
+  public string Name { get; set; } = default!;
+  public int CPF { get; set; }
+  [MaxLength(30)]
+  public string Email { get; set; } = default!;
+  [MaxLength(20)]
+
+  public string Password { get; set; } = default!;
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+  public DateTime UpdatedAt { get; set; } = DateTime.Now;
+  public ICollection<Registration>? Registrations { get; set; }
+}
